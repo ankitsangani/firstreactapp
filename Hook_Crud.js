f1ff4c3d02cbc3035a96e735bd3a157c8478f3af
ankitsangani <74649095+ankitsangani@users.noreply.github.com>
2020-12-02 10:37:20 +0530

    Add files via upload

---
+++ Hook_Crud.js
@@ -0,0 +1,58 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+const Hook_Crud = () => {
+    return(
+
+        <div className="container">
+            <button className="btn-primary" type="button" data-target="#hideen" data-toggle="collapse" aria-expanded="false" aria-controls="collapseExample">Add New</button>
+            <div className="row">
+                <div className="table-responsive">
+                    <table className="table">
+                        <thead>
+                            <tr className="table-striped">
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>Age</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table><br/>
+                </div>
+            </div>
+            <div className="row>">
+
+            <div className="col-md-6" id="hideen">
+                <h2>Registration Form</h2> <br/>
+                First name: <br/>
+                <input className="form-control" name="firstname"  type="text" placeholder="Enter YourFirst Name"/><br/>
+                Last name: <br/>
+                <input className="form-control" name="lastname" type="text" placeholder="Enter YourLast Name"/><br/>
+                Age :<br/>
+                <input className="form-control" type="text" name="age" placeholder="Enter Your Age"/><br/>
+                Gender : <br/>
+                <input  type="radio"  name="gender" value="male"/>Male
+                <input  type="radio" name="gender" value="female"/>Female
+                <input  type="radio" name="gender" value="others"/>Others<br/><br/>
+                Address : <br/>
+                <textarea className="form-control" cols="25" rows="4" placeholder="Enter Your Address">
+                </textarea>
+                Country : <br/>
+                <select className="form-control" name="country" id="country" required>
+                    <option>Select Your Country</option>
+                    <option value="India">India</option>
+                    <option value="Brazil">Brazil</option>
+                    <option value="USA">USA</option>
+                    <option value="UK">UK</option>
+                    <option value="Dubai">Dubai</option>
+                </select><br/>
+                <input type="checkbox"/> <label>I Agree the Terms & Conditions</label><br/>
+                <button className="btn-primary">Submit</button>
+                  </div>
+            </div>
+    </div>
+    );
+}
+export default Hook_Crud;
